3d7b8943ff4aca5cea6ec42b69a56d8e63cdd1cb
Jerson Romero <jersoncloud25@gmail.com>
2025-12-08 20:56:49 +0000

    updates in variables an types

--- Exercises/day-01/Python-essential-training/Quick-start/Variables-and-types.py
+++ Exercises/day-01/Python-essential-training/Quick-start/Variables-and-types.py
@@ -2,5 +2,45 @@
 x = 5 
 name = 'jerson'
 print(name)
-
 print(x)
+
+
+print (type(name))
+print (type(x))
+
+
+# Defining Numbers
+
+print (type (1.5))
+print (type(2j))
+
+x = 5
+y = 10
+z = x + y
+
+print(z)
+print (type(z))
+
+# defining strings
+
+a = 'string 1 ' + 'string 2'
+print(a)
+
+b = '1' + '1'
+print(b)
+
+#c = '1' + 1 #TypeError: can only concatenate str (not "int") to str
+#print(c)
+
+#defining booleans 
+
+true = True 
+
+print(true)
+
+d = 1==1
+print(d)
+
+e = 1==2
+print(e)
+
