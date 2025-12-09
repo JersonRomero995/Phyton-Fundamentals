8a6b51a88a7e7b3a77b4b29655d60c223e3a96a6
Jerson Romero <jersoncloud25@gmail.com>
2025-12-09 16:43:22 +0000

    ok with changes

---
+++ Exercises/day-01/Python-essential-training/Quick-start/fuctions.py
@@ -0,0 +1,28 @@
+# Functions
+print('hello world')
+
+def multiplybythree(val):
+    return 3 * val
+
+print(multiplybythree(2))
+
+def multiply(val1, val2):
+    return val1 * val2
+
+print(multiply(2, 4))
+
+
+a=[1,2,3,4,5]
+
+def appendlast(my_list):
+    my_list.append(6)
+
+appendlast(a)
+print(a)
+
+print(print('hello world'))
+print(type(None)) #Functions
+
+# print( None + 1) #TypeError: unsupported operand type(s) for +: 'NoneType' and 'int'
+# print( None + 1) #TypeError: unsupported operand type(s) for +: 'NoneType' and 'int'
+# print( None + 1) #TypeError: unsupported operand type(s) for +: 'NoneType' and 'int'
