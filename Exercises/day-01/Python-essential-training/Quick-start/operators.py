901537de8975ea06bd2660a0ae3f7b92e8db8fbd
Jerson Romero <jersoncloud25@gmail.com>
2025-12-09 04:12:15 +0000

    all ok

--- Exercises/day-01/Python-essential-training/Quick-start/operators.py
+++ Exercises/day-01/Python-essential-training/Quick-start/operators.py
@@ -10,3 +10,26 @@ print(25/5)
 print(20%6)
 
 print(20/6)
+
+# Arithmetic Operators with Strings
+
+print('string 1' + ' String 2')
+print(' string1 ' * 4)
+
+print( True == True)
+print(4<5)
+print(4>5)
+print(5>=1)
+
+# Logical Operators (and, or, not)
+print(True and True)
+print(True and False)
+print(not False)
+print(not True)
+
+# Membership Operators
+print('### new example starts here')
+l = [1,2,3,4,5]
+print (1 in l)
+
+print(10 not in l)
