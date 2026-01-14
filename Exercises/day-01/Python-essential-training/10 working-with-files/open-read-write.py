4dd39771125c259076cd8e4fe62a2b9b4bc2e0b5
Jerson Romero <jersoncloud25@gmail.com>
2026-01-14 16:26:05 +0000

    ok with update

--- Exercises/day-01/Python-essential-training/10 working-with-files/open-read-write.py
+++ Exercises/day-01/Python-essential-training/10 working-with-files/open-read-write.py
@@ -1,5 +1,7 @@
 #Files
 #Reading files
 
-f = open('test.txt', 'r')
+f = open('/workspaces/Phyton-Fundamentals/Exercises/day-01/Python-essential-training/10 working-with-files/test.txt', 'r')
 print(f)
+
+f.readline()
