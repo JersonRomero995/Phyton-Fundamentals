63ef1abc13a95d69f15e36ff24ed1ce5ecd3ee10
Jerson Romero <jersoncloud25@gmail.com>
2026-02-19 21:18:10 +0000

    ok

---
+++ Exercises/day-01/Python essential 1 netacad cisco/Section-2/interaction-with-user.py
@@ -0,0 +1,43 @@
+
+
+
+
+
+
+# input a float value for variable a here
+varA = float(input("insert the number for a:"))
+# input a float value for variable b here
+varB =float(input("insert the number for b:"))
+# output the result of addition here
+print("the adittion is:", varA + varB )
+# output the result of subtraction here
+print("the substraction is:", varA - varB)
+# output the result of multiplication here
+print("the multiplication is:", varA * varB)
+# output the result of division here
+print('the division is:', varA / varB)
+print("\nThat's all, folks!")
+
+
+# solution
+
+a = float(input("Enter first value: "))
+b = float(input("Enter second value: "))
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("\nThat's all, folks!")
+
+
+x = float(input("Enter value for x: "))
+
+# Write your code here.
+y = (1 /(x + 1 /(x + 1 /(x + 1 / x))))
+print("y =", y)
+
+
+#solution
+x = float(input("Enter value for x: "))
+y = 1./(x + 1./(x + 1./(x + 1./x)))
+print("y =", y)
