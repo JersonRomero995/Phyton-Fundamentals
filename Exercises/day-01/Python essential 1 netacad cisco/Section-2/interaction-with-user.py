780ba08f82196126f406fe3744b456c9a551cad7
Jerson Romero <jersoncloud25@gmail.com>
2026-02-24 20:52:36 +0000

    ok

--- Exercises/day-01/Python essential 1 netacad cisco/Section-2/interaction-with-user.py
+++ Exercises/day-01/Python essential 1 netacad cisco/Section-2/interaction-with-user.py
@@ -1,5 +1,22 @@
 
 
+#The input() function
+'''We're now going to introduce you to a completely new function, which seems to be a mirror reflection of the good old print() function.
+
+Why? Well, print() sends data to the console.
+
+The new function gets data from it.
+
+print() has no usable result. The meaning of the new function is to return a very usable result.
+
+The function is named input(). The name of the function says everything.
+
+The input() function is able to read data entered by the user and to return the same data to the running program.
+
+The program can manipulate the data, making the code truly interactive.
+
+Virtually all programs read and process data. A program which doesn't get a user's input is a deaf program.'''
+
 
 
 
