3b054f2d83e59997514bdc9a411f271d8a1c489a
Jerson Romero <jersoncloud25@gmail.com>
2026-05-27 23:04:17 +0000

    ok

--- Exercises/day-01/Python essential 1 netacad cisco/Section-3/Making-desicions-in-python.py
+++ Exercises/day-01/Python essential 1 netacad cisco/Section-3/Making-desicions-in-python.py
@@ -111,4 +111,19 @@ Look at this simple example:'''
 current_velocity_mph < 85  # Less than
 current_velocity_mph <= 85  # Less than or equal to
 
+# 3.1.5 Making use of the answers
 
+'''
+What can you do with the answer (i.e., the result of a comparison operation) you get from the computer?
+
+There are at least two possibilities: first, you can memorize it (store it in a variable) and make use of it later. How do you do that? Well, you use an arbitrary variable like this:'''
+
+answer = number_of_lions >= number_of_lionesses
+
+'''The content of the variable will tell you the answer to the question asked.
+
+The second possibility is more convenient and far more common: you can use the answer you get to make a decision about the future of the program.
+
+You need a special instruction for this purpose, and we'll discuss it very soon.
+
+Now we need to update our priority table, and put all the new operators into it.'''
